fbf5ddb68e1c37e8c13398b63eadee29f36da096
Erno Palonheimo <esp@iki.fi>
2019-07-26 06:22:54 -0300

    setenv, mkdtemp, loadavg fix, possibly working exepath

--- src/unix/irix.c
+++ src/unix/irix.c
@@ -24,10 +24,35 @@
 
 #include <stdio.h>
 #include <sys/sysinfo.h>
+#include <sys/sysget.h>
 #include <sys/sysmp.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
+
+/* Support functions for things missing from IRIX. */
+
+int
+setenv(const char *name, const char *value, int o)
+{
+    size_t len = strlen(name) + strlen(value) + 1;
+    char *s = malloc(len+1);
+    int ret;
+
+    snprintf(s, len, "%s=%s", name, value);
+    ret = putenv(s);
+    free(s);
+    return ret;
+}
+
+char *mkdtemp(char *template)
+{
+    mkdir(mktemp(template), 0700);
+    return template;
+}
+
+/* Actual libuv functions. */
 
 uint64_t uv__hrtime(uv_clocktype_t type)
 {
@@ -77,7 +102,8 @@ void uv_loadavg(double avg[3])
     
   static unsigned long avenrun_offset;
   sgt_cookie_t cookie;
-    
+
+  int i;
     
   if ((avenrun_offset = sysmp(MP_KERNADDR, MPKA_AVENRUN)) == -1) {
     avg[0] = 0.; avg[1] = 0.; avg[2] = 0.;
@@ -93,8 +119,91 @@ void uv_loadavg(double avg[3])
     return;
   }
 
-  for (int i = 0; i < 3; i++) {
+  for (i = 0; i < 3; i++) {
     avg[i] = avenrun[i];
     avg[i] /= 1024.0;
+  }
+}
+
+/* Stolen from AIX.
+ */
+/*
+ * We could use a static buffer for the path manipulations that we need outside
+ * of the function, but this function could be called by multiple consumers and
+ * we don't want to potentially create a race condition in the use of snprintf.
+ * There is no direct way of getting the exe path in AIX - either through /procfs
+ * or through some libc APIs. The below approach is to parse the argv[0]'s pattern
+ * and use it in conjunction with PATH environment variable to craft one.
+ */
+int uv_exepath(char* buffer, size_t* size) {
+  char args[PATH_MAX];
+  char abspath[PATH_MAX];
+  size_t abspath_size;
+
+  if (buffer == NULL || size == NULL || *size == 0)
+    return UV_EINVAL;
+
+  /*
+   * Possibilities for args:
+   * i) an absolute path such as: /home/user/myprojects/nodejs/node
+   * ii) a relative path such as: ./node or ../myprojects/nodejs/node
+   * iii) a bare filename such as "node", after exporting PATH variable
+   *     to its location.
+   */
+
+  /* Case i) and ii) absolute or relative paths */
+  if (strchr(args, '/') != NULL) {
+    if (realpath(args, abspath) != abspath)
+      return UV__ERR(errno);
+
+    abspath_size = strlen(abspath);
+
+    *size -= 1;
+    if (*size > abspath_size)
+      *size = abspath_size;
+
+    memcpy(buffer, abspath, *size);
+    buffer[*size] = '\0';
+
+    return 0;
+  } else {
+    /* Case iii). Search PATH environment variable */
+    char trypath[PATH_MAX];
+    char *clonedpath = NULL;
+    char *token = NULL;
+    char *path = getenv("PATH");
+
+    if (path == NULL)
+      return UV_EINVAL;
+
+    clonedpath = uv__strdup(path);
+    if (clonedpath == NULL)
+      return UV_ENOMEM;
+
+    token = strtok(clonedpath, ":");
+    while (token != NULL) {
+      snprintf(trypath, sizeof(trypath) - 1, "%s/%s", token, args);
+      if (realpath(trypath, abspath) == abspath) {
+        /* Check the match is executable */
+        if (access(abspath, X_OK) == 0) {
+          abspath_size = strlen(abspath);
+
+          *size -= 1;
+          if (*size > abspath_size)
+            *size = abspath_size;
+
+          memcpy(buffer, abspath, *size);
+          buffer[*size] = '\0';
+
+          uv__free(clonedpath);
+          return 0;
+        }
+      }
+      token = strtok(NULL, ":");
+    }
+    uv__free(clonedpath);
+
+    /* Out of tokens (path entries), and no match found */
+    return UV_EINVAL;
   }
 }
